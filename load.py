08b11c2f5c3ca700ed93fe9d18c52091c9ae1ab0
Ian Clark <i.clark@gen.cam.ac.uk>
2022-05-19 20:39:09 +0100

    Tidying, refactoring. Loads but probably doesn't work
    Create main object for globals (like other plugins)
    Split off UI creation to function, should allow separate window in the
    future
    Draft EDSM and cache loading
    Rough and ready mapping between journal data and EDSM format
    Remove tree filling from event handler in preparation for new function

--- load.py
+++ load.py
@@ -2,6 +2,7 @@ import logging
 import os
 import semantic_version
 from typing import Optional, Dict, Any
+import json
 
 import tkinter as tk
 from tkinter import ttk
@@ -9,12 +10,58 @@ import myNotebook as nb
 from theme import theme
 
 from config import appname, appversion
+import timeout_session
 
 plugin_name=os.path.basename(os.path.dirname(__file__))
 logger=logging.getLogger(f'{appname}.{plugin_name}')
 
-head: Optional[ttk.Label] = None
-tree: Optional[ttk.Treeview] = None
+SCAN_STAR_TO_EDSMBODY={
+        "BodyName":"name",
+        "BodyID":"bodyID",
+        "Parents":"parents",
+        "DistanceFromArrivalLS":"distanceToArrival", 
+        "StarType":"subType", # rough match
+        "Subclass":"spectralClass", # very rough match
+        "StellarMass":"solarMasses",
+        "Radius":"solarRadius", # units wrong
+        "AbsoluteMagnitude":"absoluteMagnitude",
+        "Age_MY":"age",
+        "SurfaceTemperature":"surfaceTemperature",
+        "Luminosity":"luminosity",
+        "SemiMajorAxis":"semiMajorAxis",
+        "Eccentricity":"orbitalEccentricity",
+        "OrbitalInclination":"orbitalInclination",
+        "Periapsis":"argOfPeriapsis",
+        "OrbitalPeriod":"orbitalPeriod",
+        "RotationalPeriod":"rotationalPeriod", # units wrong
+        "AxialTilt":"axialTilt"
+        }
+SCAN_PLANET_TO_EDSMBODY={
+        "BodyName":"name",
+        "BodyID":"bodyID",
+        "Parents":"parents",
+        "DistanceFromArrivalLS":"distanceToArrival",
+        "TidalLock":"rotationalPeriodTidallyLocked",
+        "TerraformState":"terraformingState",
+        "PlanetClass":"subType", # rough match
+        "AtmosphereType":"atmosphereType", # rough match
+        "AtmosphereComposition":"atmosphereComposition", # rough match
+        "Volcanism":"volcanismType",
+        "MassEM":"earthMasses",
+        "Radius":"radius", # units wrong
+        "SurfaceGravity":"gravity",
+        "SurfaceTemperature":"surfaceTemperature",
+        "SurfacePressure":"surfacePressure",
+        "Landable":"isLandable",
+        "Composition":"solidComposition",
+        "SemiMajorAxis":"semiMajorAxis",
+        "Eccentricity":"orbitalEccentricity",
+        "OrbitalInclination":"orbitalInclination",
+        "OrbitalPeriod":"orbitalPeriod",
+        "RotationalPeriod":"rotationalPeriod", # units wrong
+        "AxialTilt":"axialTilt"
+        }
+
 
 if not logger.hasHandlers():
     level = logging.INFO
@@ -26,69 +73,63 @@ if not logger.hasHandlers():
     logger_channel.setFormatter(logger_formatter)
     logger.addHandler(logger_channel)
 
+class This:
+    def __init__(self):
+        self.frame: tk.Frame = None
+        self.header: ttk.Label = None
+        self.tree: ttk.Treeview = None
+        self.session = timeout_session.new_session()
+        self.systeminfo: Dict = {}
+
+this = This()
+
 def plugin_start3(plugin_dir: str) -> str:
     return plugin_name
 
 def plugin_app(parent: tk.Frame) -> tk.Frame:
-    global tree
-    frame = tk.Frame(parent)
-    frame.rowconfigure(1,weight=1)
-    frame.rowconfigure(0,weight=1)
-    frame.columnconfigure(0,weight=1)
-    head = ttk.Label(frame,wraplength=200,justify="left",text="")
-    head.grid(row=0,sticky=tk.EW)
-    tree = ttk.Treeview(frame,columns=('Class'))
-    tree.column("#0",width=100,minwidth=50,stretch="yes");
-    tree.column("Class",width=100,minwidth=50,stretch="yes");
-    tree.heading('#0',text="Body Name")
-    tree.heading('Class',text="Class")
-    tree.grid(row=1,sticky=tk.EW)
-    theme.update(frame)
-    return frame
+    this.frame = tk.Frame(parent)
+    draw_UI()
+    return this.frame
 
 def journal_entry(cmdr: str, is_beta: bool, system: str, station: str, entry: Dict[str,Any], state: Dict[str, Any]) -> None:
-    global tree
-
     # Think about using match
-    if entry['event'] in ('FSDJump','CarrierJump'):
-        # We have arrived somewhere
-        # clear body list. 
-        # Maybe get data from edsm, eddiscovery seems to do this. 
-        #   https://www.edsm.net/api-system-v1/bodies?systemName={name}
-        #   https://www.edsm.net/api-system-v1/bodies?systemId64={id}
-        # Maybe a sqlite cache? Or just json on disk?
-        # Useful info:
-        #   StarSystem, SystemAddress
-        for item in tree.get_children():
-            tree.delete(item)
+    if entry['event'] in ('FSDJump','CarrierJump','Location'):
+        # We have loaded/died/arrived somewhere
+
+        # Get cached data, fallback to EDSM
+        get_bodies(event['SystemAddress'])
+
+        # clear tree 
+        for item in this.tree.get_children():
+            this.tree.delete(item)
+        if not this.systeminfo['bodyCount']:
+            this.header.config(text='Honk required')
+        else:
+            this.header.config(text=f'Astro: {this.systeminfo["bodies"].len()}/{systeminfo["bodyCount"]}')
+        # fill tree
+        fill_Tree()
         return None
+
     if entry['event'] == 'FSSDiscoveryScan':
         # honk!
-        # Useful info:
-        #   Bodycount, SystemName, SystemAddress, Progress
-        head.config(text=f'{event["progress"]*event["bodycount"]}/{event["Bodycount"]}')
+        this.header.config(text=f'0/{event["BodyCount"]}')
+        this.systeminfo["bodyCount"]=event["BodyCount"]
         return None
-    # more detailed scan, Basic, Detailed, NavBeacon, NavBeaconDetail, AutoScan
+    # more detailed scan, 
     if entry['event'] == 'Scan':
         # Useful info:
-        #   BodyName, BodyID, Parents[0], SystemAddress, PlanetClass, Atmosphere,
+        #   BodyName, BodyID, Parents, SystemAddress, PlanetClass, Atmosphere,
         #   Volcanism, MassEM, Radius, Landable, WasDiscovered, WasMapped
-
+        #   ScanType (Basic, Detailed, NavBeacon, NavBeaconDetail, AutoScan)
+        body={}
         if "PlanetClass" in entry.keys():
-            if tree.exists(entry['BodyID']):
-                # update
-                tree.item(entry['BodyID'],text=entry['BodyName'],values=(entry['PlanetClass'],entry['DistanceFromArrivalLS']))
-            else:
-                parentid=parental_placeholders(entry['Parents'])
-                tree.insert(parentid,0,iid=entry['BodyID'],open=True,text=entry['BodyName'],values=(entry['PlanetClass'],entry['DistanceFromArrivalLS']))
+            for scankey,edsmkey in SCAN_PLANET_TO_EDSMBODY:
+                body[edsmkey]=entry[scankey]
         elif "StarType" in entry.keys():
-            if tree.exists(entry['BodyID']):
-                # update
-                tree.item(entry['BodyID'],text=entry['BodyName'],values=(entry['StarType'],entry['DistanceFromArrivalLS']))
-            else:
-                parentid=parental_placeholders(entry['Parents'])
-                tree.insert(parentid,0,iid=entry['BodyID'],open=True,text=entry['BodyName'],values=(entry['StarType'],entry['DistanceFromArrivalLS']))
-
+            for scankey,edsmkey in SCAN_STAR_TO_EDSMBODY:
+                body[edsmkey]=entry[scankey]
+        this.systeminfo['bodies'].push(body)
+        fill_Tree()
         return None
     if entry['event'] == "FSSBodySignals":
         # Surface stuff, e.g. bio/vulc.
@@ -100,6 +141,7 @@ def journal_entry(cmdr: str, is_beta: bool, system: str, station: str, entry: Di
     if entry['event'] == 'SAAScanComplete': 
         # Useful info:
         #  BodyID
+        # Find the bodyid, set "dssScanned: true"
         return None
     # centre of gravity of pair
     if entry['event']=='ScanBaryCentre': 
@@ -114,16 +156,56 @@ def parental_placeholders(parents: list) -> int:
 
     If we don't already have the parent(s) in the tree we'll add placeholders
     """
-    global tree
     parentiid=0
     # Find out if parent exists. If not, create
     for p in reversed(parents):
         for obj,iid in p.items():
-            if not tree.exists(iid):
+            if not this.tree.exists(iid):
                 if parentiid == iid:
-                    tree.insert('',0,iid=iid,open=True,text="?",values=(f'{obj}'))
+                    this.tree.insert('',0,iid=iid,open=True,text="?",values=(f'{obj}'))
                 else:
-                    tree.insert(parentiid,0,iid=iid,open=True,text="?",values=(f'{obj}'))
+                    this.tree.insert(parentiid,0,iid=iid,open=True,text="?",values=(f'{obj}'))
             parentiid=iid
     return parentiid
 
+def get_edsm_bodies(systemid: int) -> None:
+    try:
+        r=session.get(f'https://www.edsm.net/api-system-v1/bodies?systemId64={systemid}')
+        r.raise_for_status()
+        this.system=r.json()
+        for body in this.system['bodies']:
+            body['scanType']="EDSM"
+    except Exception as e:
+        logger.warning('Problem when getting system info from EDSM')
+
+def get_cached_bodies(systemid: int) -> bool:
+    # open a file lol.
+    file=f'{systemid:016x}.json'
+    subdir=file[0,1]
+    if os.path.exists(f'data/{subdir}/{file}'):
+        with open(f'data/{subdir}/{file}', r) as f:
+            this.systeminfo=json.load(f)
+            return True
+    return False
+
+def get_bodies(systemid: int) -> None:
+    if not get_cached_bodies(systemid):
+        get_edsm_bodies(systemid)
+
+def draw_UI()->None:
+    this.frame.rowconfigure(1,weight=1)
+    this.frame.rowconfigure(0,weight=1)
+    this.frame.columnconfigure(0,weight=1)
+    this.header = ttk.Label(this.frame,wraplength=200,justify="left",text="D O R A - Awaiting initialisation")
+    this.header.grid(row=0,sticky=tk.EW)
+    this.tree = ttk.Treeview(this.frame,columns=('Class'))
+    this.tree.column("#0",width=100,minwidth=50,stretch="yes");
+    this.tree.column("Class",width=100,minwidth=50,stretch="yes");
+    this.tree.heading('#0',text="Body Name")
+    this.tree.heading('Class',text="Class")
+    this.tree.grid(row=1,sticky=tk.EW)
+    theme.update(this.frame)
+
+def fill_Tree()->None:
+    # fill the tree from this.systeminfo.
+    return None
